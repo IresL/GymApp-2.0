e9044e7dd6f93b9c5ea21a0b89fdb188b9f7f8fb
Irakli_lezhava <ik.lejava@gmail.com>
2025-08-25 18:46:21 +0400

    test

--- src/main/java/com/gym/gymapp/GymApplication.java
+++ src/main/java/com/gym/gymapp/GymApplication.java
@@ -4,7 +4,7 @@ import com.gym.gymapp.config.RootConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
+//
 public class GymApplication {
 	private static final Logger log = LoggerFactory.getLogger(GymApplication.class);
 
